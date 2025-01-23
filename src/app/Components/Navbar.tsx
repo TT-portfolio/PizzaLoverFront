5a425a60a0ff6cb58b22c73df51161e0a9de44c0
Thomas Hallström <minimoto@hotmail.com>
2025-01-23 14:29:53 +0100

    Cleaning in code

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,19 +1,9 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
-// import { useApi } from "@/context/ApiContext";
 
 export default function Navbar() {
-    const pathname = usePathname();
-    const [dropdownOpen, setDropdownOpen] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
-    // const { data, loading, error } = useApi();
-
-    // if (loading) return <p>Loading...</p>;
-    // if (error) return <p className="text-red-500">Error: {error}</p>;
-
-    // const cars = data.items.filter((item) => item.contentType === "car");
 
     return (
         <nav className="bg-background text-white shadow-lg relative">
@@ -21,7 +11,9 @@ export default function Navbar() {
                 <div className="flex items-center h-16 justify-between">
                     {/* Logo */}
                     <Link href="/">
-                        <div className="text-xl font-bold text-color-text-red">Logo</div>
+                        <div className="text-xl font-bold text-color-text-red">
+                            Logo
+                        </div>
                     </Link>
                     {/* Länkar från tablet och uppåt */}
                     <ul className="hidden md:flex md:gap-4 md:pr-4 text-color-text-red font-semibold text-lg ">
@@ -64,19 +56,22 @@ export default function Navbar() {
                                 </svg>
                             )}
                         </button>
-                        
-                        {menuOpen ? ( 
-                            <ul className="md:hidden flex flex-col bg-color-hamburger-bg text-black font-semibold p-4 absolute top-20 right-4 left-4 text-center rounded-lg"onClick={() => setMenuOpen(!menuOpen)}>
-                                
-                        <Link href="#">Hem</Link>
-                        <Link href="#">Meny</Link>
-                        <Link href="#">Kontakt</Link>
-                    </ul>
-                        ): (<ul className="hidden">
-                            <Link href="#">Hem</Link>
-                            <Link href="#">Meny</Link>
-                            <Link href="#">Kontakt</Link>
-                        </ul>)}
+
+                        {menuOpen ? (
+                            <ul
+                                className="md:hidden flex flex-col bg-color-hamburger-bg text-black font-semibold p-4 absolute top-20 right-4 left-4 text-center rounded-lg"
+                                onClick={() => setMenuOpen(!menuOpen)}>
+                                <Link href="#">Hem</Link>
+                                <Link href="#">Meny</Link>
+                                <Link href="#">Kontakt</Link>
+                            </ul>
+                        ) : (
+                            <ul className="hidden">
+                                <Link href="#">Hem</Link>
+                                <Link href="#">Meny</Link>
+                                <Link href="#">Kontakt</Link>
+                            </ul>
+                        )}
                     </div>
                 </div>
             </div>

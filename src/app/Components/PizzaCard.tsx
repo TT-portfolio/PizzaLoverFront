fe7ef1a23f625dd53cda7b0faf7d8b28aecc5ba4
Thomas Hallström <minimoto@hotmail.com>
2025-01-29 22:32:35 +0100

    Adding image to pizzaCard

--- src/app/Components/PizzaCard.tsx
+++ src/app/Components/PizzaCard.tsx
@@ -6,12 +6,16 @@ interface PizzaCardProps {
 }
 
 export default function PizzaCard({pizza}: PizzaCardProps) {
-  const { pizzaName, pizzaDescription, pizzaPrice} = pizza.properties;
+  const { pizzaName, pizzaDescription, pizzaPrice, image} = pizza.properties;
+
+  console.log("image data", image)
+  
   return (
     <div className='flex flex-col gap-0'>
         <p className='text-color-text-red font-semibold p-0'>{pizzaName}</p>
         <p className='font-medium'>{pizzaDescription}</p>
         <p className='font-medium'>{pizzaPrice} :-</p>
+        {image?.[0]?.url && <p>{image[0].url}</p>}
         <div className='h-px bg-gray-400'></div>
     </div>
   )
--- src/types/pizza.ts
+++ src/types/pizza.ts
@@ -1,21 +1,22 @@
 // types/pizza.ts
 export interface Pizza {
-  id: string;
-  name: string;
-  createDate: string;
-  updateDate: string;
-  route: {
-    path: string;
-    startItem: {
-      id: string;
-      path: string;
+    id: string;
+    name: string;
+    createDate: string;
+    updateDate: string;
+    route: {
+        path: string;
+        startItem: {
+            id: string;
+            path: string;
+        };
     };
-  };
-  properties: {
-    pizzaDescription: string;
-    pizzaName: string;
-    pizzaPrice: number;
-    ham: string | null;
-    ost: string | null;
-  };
+    properties: {
+        pizzaDescription: string;
+        pizzaName: string;
+        pizzaPrice: number;
+        image?: { url: string }[];
+        ham: string | null;
+        ost: string | null;
+    };
 }

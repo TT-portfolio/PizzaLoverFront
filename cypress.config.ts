79393e9a348bd6dfd3a204cb80405a75efbd7b87
MrMinken <minimoto@hotmail.com>
2025-02-07 12:57:33 +0100

    Adding time delay for database

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: process.env.BASE_URL || "https://pizzalover-g7fnhxctfsfbe6c7.westeurope-01.azurewebsites.net/",
+    defaultCommandTimeout: 20000,  // Timeout på 10 sekunder
+    requestTimeout: 20000,  // Timeout för API-anrop
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

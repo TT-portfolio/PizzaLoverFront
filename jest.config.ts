3f33b842dd3cc63e08e0add7075a94cab9833c7c
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2025-02-07 15:04:11 +0100

    Trying to fix problem with importType in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
-import type { Config } from "jest";
-import nextJest from "next/jest";
-
+import nextJest from "next/jest"; 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
-const config: Config = {
+const config = {
   clearMocks: true,
   globals: {
     fetch: global.fetch,

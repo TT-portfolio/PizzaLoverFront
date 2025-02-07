7fd6481cdb9016b18f944e5d30c0f9363cb8df6d
MrMinken <minimoto@hotmail.com>
2025-02-07 14:24:16 +0100

    Adding styling to pizza card

--- src/app/Components/PizzaCard.tsx
+++ src/app/Components/PizzaCard.tsx
@@ -5,56 +5,74 @@ import Image from "next/image";
 import Button from "../Components/Button";
 
 interface PizzaCardProps {
-    pizza: Pizza;
+  pizza: Pizza;
 }
 
 export default function PizzaCard({ pizza }: PizzaCardProps) {
-    const { pizzaName, pizzaDescription, pizzaPrice, image, ingridienser } = pizza.properties;
-    const baseUrl =
-        "https://pizzaloverstorage.blob.core.windows.net/pizzalovercontainer";
+  const { pizzaName, pizzaDescription, pizzaPrice, image, ingridienser } =
+    pizza.properties;
+  const baseUrl =
+    "https://pizzaloverstorage.blob.core.windows.net/pizzalovercontainer";
 
-    const imageFileName = image?.[0].url;
-    const imageUrl = imageFileName ? `${baseUrl}${imageFileName}` : null;
-    const [ingridiens, setIngridiens] = useState(true);
-    const [altText, setAltText] = useState<string>(
-        imageFileName ? "Loading..." : "No alt text available"
-    );
+  const imageFileName = image?.[0].url;
+  const imageUrl = imageFileName ? `${baseUrl}${imageFileName}` : null;
+  const [altText, setAltText] = useState<string>(
+    imageFileName ? "Loading..." : "No alt text available"
+  );
 
-    const [ingridienser, setIngridienser] = useState<boolean>(false);
+  const [ingrid, setIngrid] = useState<boolean>(false);
 
-    const handleClick = () => {
-        setIngridienser(prevState => !prevState);
-        console.log(ingridienser)
+  const handleClick = () => {
+    setIngrid((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (imageUrl) {
+      fetchImageMetadata(imageUrl).then(setAltText);
     }
-
-    useEffect(() => {
-        if (imageUrl) {
-            fetchImageMetadata(imageUrl).then(setAltText);
-        }
-    }, [imageUrl]);
-    return (
-        <div className="flex flex-col">
-            <div className="flex gap-2 justify-between w-full">
-                <div className="w-full">
-                    <div className="flex justify-between sm:flex-col">
-                        <p data-test="pizzaName" className="text-color-text-red font-semibold p-0">
-                            {pizzaName}
-                        </p>
-                        <p className="font-semibold">{pizzaPrice} :-</p>
-                    </div>
-                            <Button label="Ingridienser" onClick={handleClick}/>
-                    <p className="font-medium">{pizzaDescription}</p>
-                </div>
-                {imageUrl && (
-                    <Image
-                        src={imageUrl}
-                        alt={altText}
-                        width={500}
-                        height={500}
-                        className="w-40 h-40 min-w-40 object-cover rounded-md"
-                    />
+  }, [imageUrl]);
+  return (
+    <div className="flex flex-col">
+      <span>------</span>
+      <div className="flex gap-2 justify-between w-full">
+        <div className="w-full flex flex-col gap-2">
+          <div className="flex justify-between">
+            <p
+              data-test="pizzaName"
+              className="text-color-text-red font-semibold p-0"
+            >
+              {pizzaName}
+            </p>
+            <p className="font-semibold">{pizzaPrice} :-</p>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex flex-col justify-between">
+              <div>
+                {ingrid ? (
+                  <p className="font-medium">{pizzaDescription}</p>
+                ) : (
+                  ingridienser?.map((ingri, index) => (
+                    <p key={index}>{ingri}</p>
+                  )) || <p>Inga ingridienser är tillgängliga</p>
                 )}
+              </div>
+              <Button
+                label={ingrid ? "Ingridienser" : "Beskrivning"}
+                onClick={handleClick}
+              />
             </div>
+            {imageUrl && (
+              <Image
+                src={imageUrl}
+                alt={altText}
+                width={500}
+                height={500}
+                className="w-40 h-40 min-w-40 object-cover rounded-md"
+              />
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/app/Components/button.tsx
+++ src/app/Components/button.tsx
@@ -1,16 +1,10 @@
-import React from "react";
+import React from "react"
+import {ButtonProps} from "@/types/button"
 
-interface ButtonProps {
-    text: string;
-    onClick: () => void;
-}
-
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+const Button: React.FC<ButtonProps> = ({label, onClick})=> {
     return (
-        <button onClick={onClick} className="bg-gray-300 hover:bg-gray-500 p-2 rounded-md font-semibold hover:text-white">
-            {text}
+        <button onClick={onClick} className="bg-color-text-red p-1 rounded-md active:bg-color-text-green">
+            {label}
         </button>
-    );
-};
-
-export default Button;
+    )
+}; export default Button;

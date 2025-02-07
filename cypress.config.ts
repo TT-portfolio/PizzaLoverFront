b690f824a9cf48d4010b84d8b24f7acfa0541379
MrMinken <minimoto@hotmail.com>
2025-02-07 13:02:38 +0100

    correct address

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || "https://pizzalover-g7fnhxctfsfbe6c7.westeurope-01.azurewebsites.net/",
+    baseUrl: process.env.BASE_URL || "https://nice-field-0563ff303.4.azurestaticapps.net",
     defaultCommandTimeout: 20000,  // Timeout på 10 sekunder
     requestTimeout: 20000,  // Timeout för API-anrop
     setupNodeEvents(on, config) {

f94b76d63a5639d622569bd3688a25062e29ff57
MrMinken <minimoto@hotmail.com>
2025-02-07 12:18:09 +0100

    update in correct adress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: process.env.BASE_URL || "https://pizzalover-g7fnhxctfsfbe6c7.westeurope-01.azurewebsites.net/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

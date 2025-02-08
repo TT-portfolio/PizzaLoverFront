e0d1c498b3d0c826dc9e6a307134595dc0c48d29
MrMinken <minimoto@hotmail.com>
2025-02-08 10:26:48 +0100

    Adding test on Home and Menu page

--- cypress/e2e/assesebility.cy.ts
+++ cypress/e2e/assesebility.cy.ts
@@ -1,14 +1,20 @@
 import "cypress-axe"
 
-describe('Accessibility Testing', () => {
+describe('Accessibility Testing Menu page', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:3000/Menu');
+      cy.visit('http://localhost:3000');
       cy.injectAxe();  // Laddar in axe-core i sidan
     });
-  
-    it('Should have no detectable accessibility violations', () => {
-      cy.get('h1').should('be.visible'); 
+    
+    it('Should have no detectable accessibility violations on HomePage', () => {
       cy.checkA11y();  // Kör tillgänglighetstester
     });
+    
+    it('Should have no detectable accessibility violations on MenuPage', ()=> {
+      cy.visit("/Menu")
+      cy.injectAxe();  // Laddar in axe-core i sidan
+      cy.get('h1').should('be.visible'); 
+      cy.checkA11y()
+    })
   });
   

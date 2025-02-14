44d3f919af8e27c9e72465487438dc089a2d61e6
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2025-02-14 16:17:08 +0100

    Added menu to HomePage/StartPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,48 @@
-
-// import HomePage from "./HomePage/page";
-
-// export default function Home() {
-//     return (
-//         <div className="p-4">
-            
-//             <HomePage />
-//         </div>
-//     );
-// }
-
-//Denna används för att kunna rendera till sidan
 "use client";
-//Dessa delar används för att kalla på olika funktioner i koden
+
 import { useEffect, useState } from "react";
 import { useApi } from "@/context/ApiContext";
-import { Homepage } from "@/types/homepage";
+import { Pizza } from "@/types/pizza";
+import MenuStartPage from "@/app/Components/MenuStartPage"; 
 
 function HomePage() {
-  //Denna delen kallar på API för att hämta informationen
   const { fetchPage, loading, error } = useApi();
-  const [settings, setSettings] = useState<Homepage | null>(null);
+  const [pizzas, setPizzas] = useState<Pizza[]>([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const items = await fetchPage<Homepage>("homePage");
-      // Filtrera på settingsPage
-      //setSettings(items && items.length > 0 ? items[0]: null);
-      if (items){
-        setSettings(items[0]);
+    async function fetchData() {     
+      const items = await fetchPage<Pizza>("pizzaObj");
+      if (items) {
+        console.log("API Response från pizzaObj:", items); 
+        setPizzas(items);
       }
     }
     fetchData();
   }, [fetchPage]);
 
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <p>Error: {error}</p>;
-  if (!settings || !settings.properties)
-    return <p>No settings data available.</p>;
+  if (loading) return <h1 className="text-3xl">Loading...</h1>;
+  if (error) return <p className="text-2xl">Error: {error}</p>;
 
   return (
-    //Här skriver man ut det som kommer ifrån API
-    <div>
-      <p className="text-green-500 m-4">
-        {typeof settings.properties.pageTitle === "string"
-          ? settings.properties.pageTitle
-          : "Default Title"}
-      </p>
+    <div className="flex flex-col items-center justify-center w-full mt-16">
+      <div className="w-full max-w-3xl mx-auto text-center">
+        <h1 className="text-[var(--color-text-green)] text-4xl font-semibold mb-6">
+          Pizzor
+        </h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-8 max-w-3xl mx-auto px-4">
+        {pizzas.length > 0 ? (
+          pizzas.map((pizza, index) => (
+            <MenuStartPage key={index} pizza={pizza} />
+          ))
+        ) : (
+          <p className="text-xl">Inga pizzor tillgängliga.</p>
+        )}
+      </div>
     </div>
   );
 }
+
 
 export default HomePage;
